3a5cab41b1993e04cf692fc7ccacb1c8be065f6b
Hernan Garcia <hernan@dynamicprogrammer.com>
2022-02-10 17:44:52 -0500

    [HG] #1zwzw59

--- src/barcode.js
+++ src/barcode.js
@@ -13,27 +13,39 @@ function Barcode(engine) {
         this.type = args[1] || "code128";
         this.height = args[2] || 30;
         this.width = args[3] || 200;
+        this.margin = args[4] || "0,0,0,0";
       } catch (err) {
         this.content = "not-content-given";
         this.type = "code128";
         this.height = 30;
         this.width = args[3] || 200;
+        this.margin = args[4] || "0,0,0,0";
       }
     },
     render: async function(ctx) {
-      // console.log(this.content);
       let content = await this.liquid.evalValue(this.content, ctx) || this.content;
-      // console.log("content", content);
       let result = await createStream({
         symbology: getCode(this.type),
         height: this.height,
         showHumanReadableText: false,
         scale: 2.0,
       }, String(content), OutputType.PNG);
-      // console.log("RESULT:", result);
+      let margin = [0,0,0,0];
+      try {
+        margin = this.margin.split(",").map((i) => {
+          return parseInt(i,10);
+        });
+        if (margin.length !== 4) {
+          margin = [0,0,0,0];
+        }
+      } catch (err) {
+        console.log("ERROR:", err);
+        margin = [0,0,0,0];
+      }
       return `{
         "image": "${result.data}",
-        "width": ${this.width || 200}
+        "width": ${this.width || 200},
+        "margin": [${margin.join(",")}]
       }`;
     }
 });
