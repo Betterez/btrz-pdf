6a981aa9478141a8a4670f296e227ab3093a0c3d
Hernan Garcia <hernan@dynamicprogrammer.com>
2022-02-25 14:26:33 -0500

    [HG] fix solditem and flexpass qrstr

--- src/qrstr.js
+++ src/qrstr.js
@@ -1,25 +1,25 @@
 function QrString(engine) {
   this.registerTag("qrstr", {
     parse: function(tagToken, remainTokens) {
-      this.str = tagToken.args || "ticket";
+      this.str = (tagToken.args || "ticket").toLowerCase();
     },
     render: async function(ctx) {
       if (this.str === "reservation" || this.str === "ticket") {
         return `https://${ctx.environments.providerPreferences.domain}/r/t/${ctx.environments[this.str].urlTicketCode}`;
       }
-      if (this.str === "paid_in") {
-        return `PI-${ctx.environments.paidIn._id.toString()}`;
+      if (this.str === "solditem") {
+        return `PI-${ctx.environments.soldItem._id.toString()}`;
       }
       if (this.str === "parcel") {
         return `${ctx.environments.parcel.accountId}-PA-${ctx.environments.parcel._id.toString()}`;
       }
       if (this.str === "flexpass") {
-        return `${ctx.environments.flexpass.ticketNumber}`;
+        return `${ctx.environments.flexPass.ticketNumber}`;
       }
       if (this.str === "ssr") {
         return `${ctx.environments.ssr.ssrsInstanceId}`;
       }
-      if (this.str === "redeemableItem") {
+      if (this.str === "redeemableitem") {
         return `RI-${ctx.environments.redeemableItem._id.toString()}`;
       }
       return ""
